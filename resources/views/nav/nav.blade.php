7b32b57ae36ffeea06217d451ab1c5ab127df608
kraved <kraved>
2020-07-06 17:34:22 +0300

    Добавлена ссылка на IT Заявки

--- resources/views/nav/nav.blade.php
+++ resources/views/nav/nav.blade.php
@@ -9,8 +9,8 @@
             <div class="nav-item"><a href="{{ route('documents.index') }}">Шаблоны документов</a></div>
             <div class="nav-item"><a href="{{ route('addressbook.index') }}">Адресная книга</a></div>
             <div class="nav-item"><a href="{{ route('birthdays.index') }}">Дни Рождения</a></div>
-            <div class="nav-item"><a href="">Полезные ссылки</a></div>
-            <div class="nav-item"><a href="">Заявки в IT</a></div>
+            <div class="nav-item"><a href="{{ route('links.index') }}">Полезные ссылки</a></div>
+            <div class="nav-item"><a href="https://help2.admin24.ru/f4ae33f3cd0f0d3b49509396972f7eb9">Заявки в IT</a></div>
             <div class="nav-item">
                 <a class="nav-item-menu" href=""><img class="ellipsis-icon" src="{{ asset('images/ellipsis.svg') }}"
                                                       alt="ellipsis">

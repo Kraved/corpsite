3ec5aaeab59a0f1f071b9c4e5c9a49bffabb5b6e
kraved <kraved>
2020-07-09 11:12:07 +0300

    добавлены стили на error и success блоки.

--- resources/views/layouts/includes/success_block.blade.php
+++ resources/views/layouts/includes/success_block.blade.php
@@ -1,3 +1,6 @@
-{{ session('success') }}
-
-
+<div class="alert alert-success alert-dismissible fade show text-center" role="alert">
+    {{ session('success') }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>

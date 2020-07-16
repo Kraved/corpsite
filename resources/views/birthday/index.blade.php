bee791f47c3034c9643a879dc45137574bd5176c
kraved <kraved>
2020-07-16 13:21:27 +0300

    Добавлены стили на запись text в birthday.index

--- resources/views/birthday/index.blade.php
+++ resources/views/birthday/index.blade.php
@@ -5,51 +5,51 @@
     <h1 class="content-title">Дни Рождения</h1>
     <article class="birthday">
         <h1 class="birthday-title">Январь</h1>
-        {{ $data['January']->text }}
+        <div class="birthday-text">{!! $data['January']->text !!}</div>
     </article>
     <article class="birthday">
         <h1 class="birthday-title">Февраль</h1>
-        {{ $data['February']->text }}
+        <div class="birthday-text">{!! $data['February']->text !!}</div>
     </article>
     <article class="birthday">
         <h1 class="birthday-title">Март</h1>
-        {{ $data['March']->text }}
+        <div class="birthday-text">{!! $data['March']->text !!}</div>
     </article>
     <article class="birthday">
         <h1 class="birthday-title">Апрель</h1>
-        {{ $data['April']->text }}
+        <div class="birthday-text">{!! $data['April']->text !!}</div>
     </article>
     <article class="birthday">
         <h1 class="birthday-title">Май</h1>
-        {{ $data['May']->text }}
+        <div class="birthday-text">{!! $data['May']->text !!}</div>
     </article>
     <article class="birthday">
         <h1 class="birthday-title">Июнь</h1>
-        <div class="birthday-text">{{ $data['June']->text }}</div>
+        <div class="birthday-text">{!! $data['June']->text !!}</div>
     </article>
     <article class="birthday">
         <h1 class="birthday-title">Июль</h1>
-        {{ $data['July']->text }}
+        <div class="birthday-text">{!! $data['July']->text !!}</div>
     </article>
     <article class="birthday">
         <h1 class="birthday-title">Август</h1>
-        {{ $data['August']->text }}
+        <div class="birthday-text">{!! $data['August']->text !!}</div>
     </article>
     <article class="birthday">
         <h1 class="birthday-title">Сентябрь</h1>
-        {{ $data['September']->text }}
+        <div class="birthday-text">{!! $data['September']->text !!}</div>
     </article>
     <article class="birthday">
         <h1 class="birthday-title">Октябрь</h1>
-        {{ $data['October']->text }}
+        <div class="birthday-text">{!! $data['October']->text !!}</div>
     </article>
     <article class="birthday">
         <h1 class="birthday-title">Ноябрь</h1>
-        {{ $data['November']->text }}
+        <div class="birthday-text">{!! $data['November']->text !!}</div>
     </article>
     <article class="birthday">
         <h1 class="birthday-title">Декабрь</h1>
-        {{ $data['December']->text }}
+        <div class="birthday-text">{!! $data['December']->text !!}</div>
     </article>
 </section>
 

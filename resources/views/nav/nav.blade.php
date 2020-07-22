d5ccb2425ebb9a041b30c635c2619dfd3b4481b4
kraved <kraved>
2020-07-22 11:09:05 +0300

    Менеджмент панель в панели навигации смещение вниз

--- resources/views/nav/nav.blade.php
+++ resources/views/nav/nav.blade.php
@@ -15,12 +15,21 @@
                 <div class="nav-item"><a href="{{ route('login') }}">Войти</a></div>
                 <div class="nav-item"><a href="{{ route('register') }}">Регистрация</a></div>
             @endguest
+
             @auth
                 <div class="nav-item"><a href="https://help2.admin24.ru/f4ae33f3cd0f0d3b49509396972f7eb9">Заявки в IT</a></div>
-                <div class="nav-item">
-                    <a class="nav-item-menu" href=""><img class="ellipsis-icon" src="{{ asset('images/ellipsis.svg') }}"
-                                                          alt="ellipsis">
+
+                <div class="nav-item dropdown">
+                    <a class="nav-item-menu" data-toggle="dropdown" href="">
+                        <img class="ellipsis-icon" src="{{ asset('images/ellipsis.svg') }}" alt="ellipsis">
                     </a>
+                    <div class="dropdown-menu management-dropdown-menu">
+                        <a class="dropdown-item" href="{{ route('news.management.index') }}">Новости</a>
+                        <a class="dropdown-item" href="{{ route('documents.management.index') }}">Документы</a>
+                        <a class="dropdown-item" href="{{ route('addressbook.management.index') }}">Адресная книга</a>
+                        <a class="dropdown-item" href="{{ route('birthday.management.index') }}">Дни Рождения</a>
+                        <a class="dropdown-item" href="{{ route('links.management.index') }}">Ссылки</a>
+                    </div>
                 </div>
                 <div class="nav-item"><a href="{{ route('logout') }}"
                                          onclick="event.preventDefault();
@@ -34,3 +43,9 @@
 
     </div>
 </nav>
+{{--<div class="dropdown">--}}
+{{--    <button class="btn btn-primary " type="button"  >--}}
+{{--        Кнопка выпадающего списка--}}
+{{--    </button>--}}
+
+{{--</div>--}}

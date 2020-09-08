69d7cf2740f143022638532cb0b3f592cd5270c5
kraved <kraved>
2020-09-08 13:33:46 +0300

    Добавлены условия для отображения доп инфы(ошибки, успех)

--- resources/views/layouts/includes/errors_block.blade.php
+++ resources/views/layouts/includes/errors_block.blade.php
@@ -1,10 +1,14 @@
-
-<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
-    {{$errors->first()}}
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
-
-
+{{-- Для тестов--}}
+@if(isset($errors))
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            <div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
+                {{$errors->first()}}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endforeach
+    @endif
+@endif
 
